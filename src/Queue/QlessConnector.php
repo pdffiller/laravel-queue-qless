385eb18010ffc647ca28607bea418d8c3b4d9877
Dmytro Dubinin <dubinin.dmytro@airslate.com>
2021-03-10 10:22:20 +0200

    [CORE-3407] Add sharding host parser

--- src/Queue/QlessConnector.php
+++ src/Queue/QlessConnector.php
@@ -13,22 +13,106 @@ use Qless\Client;
  */
 class QlessConnector implements ConnectorInterface
 {
+    private const REDIS_CONNECTION_CONFIG_KEY = 'redis_connection';
+    private const CONFIG_PATH_PREFIX = 'database.redis.';
+
+    private const DEFAULT_CONNECTION_CONFIG = 'qless';
+
+    private const CONFIG_HOST_KEY = 'host';
+    private const CONFIG_PASSWORD_KEY = 'password';
+    private const CONFIG_PORT_KEY = 'port';
+    private const CONFIG_DATABASE_KEY = 'database';
+
+    private const CONFIG_SEPARATOR = ',';
+
+    public const DEFAULT_PASSWORD = null;
+    public const DEFAULT_PORT = 6379;
+    public const DEFAULT_DATABASE = 0;
+
     /**
-    * Establish a queue connection.
-    *
-    * @param array $config
-    *
-    * @return QlessQueue
-    */
+     * Establish a queue connection.
+     *
+     * @param array $config
+     *
+     * @return QlessQueue
+     */
     public function connect(array $config): QlessQueue
     {
-         $redisConnection = Arr::get($config, 'redis_connection', 'qless');
+        $redisConnection = Arr::get($config,
+            self::REDIS_CONNECTION_CONFIG_KEY,
+            self::DEFAULT_CONNECTION_CONFIG);
 
-         $redisConfig = Config::get('database.redis.' . $redisConnection, []);
+        $qlessConfig = Config::get(self::CONFIG_PATH_PREFIX . $redisConnection, []);
 
-         return new QlessQueue(
-             new Client($redisConfig),
-             $config
-         );
+        if ($this->hasSharding($qlessConfig)) {
+            $shardingConfigs = $this->getShardingConfigs($qlessConfig);
+            $qlessConfig = $shardingConfigs[rand(0, count($shardingConfigs) - 1)];
+        }
+
+        return new QlessQueue(
+            new Client($qlessConfig),
+            $config
+        );
     }
+
+    private function hasSharding(array $connectionConfig): bool
+    {
+        $hostList = $this->extractConfigValues(self::CONFIG_HOST_KEY, $connectionConfig);
+        return (count($hostList) > 1);
+    }
+
+    private function getShardingConfigs(array $connectionConfig): array
+    {
+        $shardingList = [];
+
+        $hostList = $this->extractConfigValues(self::CONFIG_HOST_KEY, $connectionConfig);
+        $portList = $this->extractConfigValues(self::CONFIG_PORT_KEY, $connectionConfig);
+        $passwordList = $this->extractConfigValues(self::CONFIG_PASSWORD_KEY, $connectionConfig);
+        $databaseList = $this->extractConfigValues(self::CONFIG_DATABASE_KEY, $connectionConfig);
+
+        foreach ($hostList as $i => $host) {
+            if ($host === '') {
+                continue;
+            }
+            $shardingList[] = $this->getQlessConfig(
+                $host,
+                $passwordList[$i] ?? null,
+                $portList[$i] ?? null,
+                $databaseList[$i] ?? null
+            );
+        }
+
+        return $shardingList;
+    }
+
+    private function extractConfigValues(string $key, array $connectionConfig): array
+    {
+        $value = $connectionConfig[$key] ?? null;
+
+        if (!$value) {
+            return [];
+        }
+
+        $values = explode(self::CONFIG_SEPARATOR, $value);
+
+        return array_map(function ($v) {
+            return trim($v) ?: null;
+        }, $values);
+    }
+
+    private function getQlessConfig(
+        string $host,
+        ?string $password,
+        ?int $port,
+        ?int $database
+    ): array
+    {
+        return [
+            self::CONFIG_HOST_KEY => $host,
+            self::CONFIG_PASSWORD_KEY => $password ?: self::DEFAULT_PASSWORD,
+            self::CONFIG_PORT_KEY => $port ?: self::DEFAULT_PORT,
+            self::CONFIG_DATABASE_KEY => $database ?: self::DEFAULT_DATABASE,
+        ];
+    }
+
 }
---
+++ tests/Queue/QlessConnectorTest.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace LaravelQless\Tests\Queue;
+
+use LaravelQless\Queue\QlessConnector;
+use LaravelQless\Queue\QlessQueue;
+use PHPUnit\Framework\TestCase;
+use Qless\Config;
+
+class QlessConnectorTest extends TestCase
+{
+
+    #region Test data
+    private const QLESS_CONFIG = [
+        'driver' => 'qless',
+        'connection' => 'qless',
+        'queue' => 'default',
+        'redis_connection' => 'qless',
+    ];
+
+    private const HOST1 = 'redis.host';
+    private const PORT1 = 3333;
+    private const PWD1 = 'PASSWD';
+    private const DB1 = 1;
+
+    private const HOST2 = 'redis.host2';
+    private const PORT2 = 4333;
+    private const PWD2 = 'PASSWD2';
+    private const DB2 = 2;
+
+    private const HOST1_CONFIG = [
+        'host' => self::HOST1,
+        'password' => self::PWD1,
+        'port' => self::PORT1,
+        'database' => self::DB1,
+    ];
+
+    private const HOST2_CONFIG = [
+        'host' => self::HOST2,
+        'password' => self::PWD2,
+        'port' => self::PORT2,
+        'database' => self::DB2,
+    ];
+
+    private const SINGLE_REDIS_CONFIG = self::HOST1_CONFIG;
+    private const SHARDING_REDIS_CONFIG_SINGLE = self::HOST2_CONFIG;
+
+    private const SHARDING_REDIS_CONFIG_MULTIPLY = [
+        'host' => self::HOST1 . ',' . self::HOST2,
+        'password' => self::PWD1 . ',' . self::PWD2,
+        'port' => self::PORT1 . ',' . self::PORT2,
+        'database' => self::DB1 . ',' . self::DB2,
+    ];
+
+    private const SHARDING_REDIS_CONFIG_MULTIPLY_SPACES = [
+        'host' => '  ' . self::HOST1 . ' , ' . self::HOST2 . '   ',
+        'password' => '   ' . self::PWD1 . '   , ' . self::PWD2 . '   ',
+        'port' => '   ' . self::PORT1 . ' , ' . self::PORT2 . '   ',
+        'database' => '  ' . self::DB1 . ' , ' . self::DB2 . '   ',
+    ];
+    #endregion
+
+    protected function setEnv(array $redisConfig)
+    {
+        $this->app['config']->set('queue.default', 'qless');
+        $this->app['config']->set('queue.connections.qless', self::QLESS_CONFIG);
+        $this->app['config']->set('database.redis.qless', $redisConfig);
+    }
+
+    protected function compareConfigs(array $expectedConfig, Config $resultingConfig)
+    {
+        $this->assertEquals($expectedConfig['host'], $resultingConfig->get('host'));
+        $this->assertEquals($expectedConfig['port'], $resultingConfig->get('port'));
+        $this->assertEquals($expectedConfig['password'], $resultingConfig->get('password'));
+        $this->assertEquals($expectedConfig['database'], $resultingConfig->get('database'));
+    }
+
+    protected function getExpectedConfigOfSelected(Config $resultConfig): array
+    {
+        switch ($resultConfig->get('host')) {
+            case self::HOST1:
+            {
+                return self::HOST1_CONFIG;
+            }
+            case self::HOST2:
+            {
+                return self::HOST2_CONFIG;
+            }
+            default:
+                $this->fail('Invalid host config');
+        }
+    }
+
+
+    public function testConnectSingleConfig()
+    {
+        $config = self::SINGLE_REDIS_CONFIG;
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+        $this->compareConfigs($config, $qlessConfig);
+    }
+
+    public function testConnectShardingSingleConfig()
+    {
+        $config = self::SHARDING_REDIS_CONFIG_SINGLE;
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+        $this->compareConfigs($config, $qlessConfig);
+    }
+
+
+    public function testConnectShardingMultiplyConfig()
+    {
+        $config = self::SHARDING_REDIS_CONFIG_MULTIPLY;
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+
+        $excpectedConfig = $this->getExpectedConfigOfSelected($qlessConfig);
+        $this->compareConfigs($excpectedConfig, $qlessConfig);
+    }
+
+
+    public function testConnectShardingMultiplyConfigWithSpaces()
+    {
+        $config = self::SHARDING_REDIS_CONFIG_MULTIPLY_SPACES;
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+
+        $excpectedConfig = $this->getExpectedConfigOfSelected($qlessConfig);
+
+        $this->compareConfigs($excpectedConfig, $qlessConfig);
+    }
+
+
+    public function testConnectShardingMultiplyConfigNoPassword()
+    {
+        $config = self::SHARDING_REDIS_CONFIG_MULTIPLY;
+        $config['password'] = null;
+
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+
+        $excpectedConfig = $this->getExpectedConfigOfSelected($qlessConfig);
+        $excpectedConfig['password'] = QlessConnector::DEFAULT_PASSWORD;
+
+        $this->compareConfigs($excpectedConfig, $qlessConfig);
+    }
+
+    public function testConnectShardingMultiplyConfigNoPort()
+    {
+        $config = self::SHARDING_REDIS_CONFIG_MULTIPLY;
+        $config['port'] = null;
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+        $excpectedConfig = $this->getExpectedConfigOfSelected($qlessConfig);
+        $excpectedConfig['port'] = QlessConnector::DEFAULT_PORT;
+
+
+        $this->compareConfigs($excpectedConfig, $qlessConfig);
+    }
+
+    public function testConnectShardingMultiplyConfigNoDatabase()
+    {
+        $config = self::SHARDING_REDIS_CONFIG_MULTIPLY;
+        $config['database'] = null;
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+        $excpectedConfig = $this->getExpectedConfigOfSelected($qlessConfig);
+        $excpectedConfig['database'] = QlessConnector::DEFAULT_DATABASE;
+
+
+        $this->compareConfigs($excpectedConfig, $qlessConfig);
+    }
+
+    public function testConnectShardingMultiplyConfigSingleValues()
+    {
+        $config = self::SHARDING_REDIS_CONFIG_MULTIPLY;
+        $config['password'] = self::PWD1;
+        $config['port'] = self::PORT1;
+        $config['database'] = self::DB1;
+
+        $this->setEnv($config);
+
+        $connector = new QlessConnector();
+        $queue = $connector->connect(self::QLESS_CONFIG);
+
+        $this->assertInstanceOf(QlessQueue::class, $queue);
+
+        $qlessConfig = $queue->getConnection()->getConfig();
+
+        switch ($qlessConfig->get('host')) {
+            case self::HOST1:
+            {
+                $excpectedConfig = self::HOST1_CONFIG;
+                break;
+            }
+            case self::HOST2:
+            {
+                $excpectedConfig = self::HOST2_CONFIG;
+                $excpectedConfig['password'] = QlessConnector::DEFAULT_PASSWORD;
+                $excpectedConfig['port'] = QlessConnector::DEFAULT_PORT;
+                $excpectedConfig['database'] = QlessConnector::DEFAULT_DATABASE;
+                break;
+            }
+            default:
+                $this->fail('Invalid host config');
+        }
+
+        $this->compareConfigs($excpectedConfig, $qlessConfig);
+    }
+
+}
